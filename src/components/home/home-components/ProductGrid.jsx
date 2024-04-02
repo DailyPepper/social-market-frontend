9f0af6c4af5109798ef2137e120dd0fc4b5aff30
Daniel Kudinov <danielkudinov17@gmail.com>
2024-04-02 13:25:23 +0300

    add pagination

--- src/components/home/home-components/ProductGrid.jsx
+++ src/components/home/home-components/ProductGrid.jsx
@@ -1,54 +1,47 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Product from '../../product/Product';
-// import Pagination from '../../pagination/Pagination'
-// import { Filter } from './filter';
-import { useState } from 'react';
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import Pagination from '../../pagination/Pagination';
+import Filters from './Filters';
 import products from './products';
-import Filters from './Filters'
+
 const ProductGrid = () => {
   const [page, setPage] = useState(1);
   const productsPerPage = 6;
 
-  const getPaginatedProducts = () => {
-    const startIndex = (page - 1) * productsPerPage;
-    const endIndex = startIndex + productsPerPage;
-    return products.slice(startIndex, endIndex);
+  const startIndex = (page - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const paginatedProducts = products.slice(startIndex, endIndex);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
   };
-  
 
   return (
-  <div className="lg:max-w-[value] mx-auto px-4 flex flex-row gap-[3rem] justify-center items-stretch">
-    <Filters/>
-    <div className="flex flex-col items-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-3 gap-4 font-ralewayFont">
-        {getPaginatedProducts().map(product => (
-          <Link key={product._id} to={`/product/${product._id}`}>
-            <Product
-              _id={product._id}
-              img={product.img}
-              productName={product.productName}
-              price={product.price}
-              color={product.color}
-            />
-          </Link>
-        ))}
-      </div>
-      <div className="flex justify-center w-full pt-[4rem] gap-[5rem]">
-        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
-          <IoIosArrowBack size={25}/>
-        </button>
-        <button
-          onClick={() => setPage(page + 1)}
-          disabled={getPaginatedProducts().length < productsPerPage}
-        >
-          <IoIosArrowForward size={25}/>
-        </button>
+    <div className="lg:max-w-[value] mx-auto px-4 flex flex-row gap-[3rem] justify-center items-stretch">
+      <Filters />
+      <div className="flex flex-col items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-3 gap-4 font-ralewayFont">
+          {paginatedProducts.map(product => (
+            <Link key={product._id} to={`/product/${product._id}`}>
+              <Product
+                _id={product._id}
+                img={product.img}
+                productName={product.productName}
+                price={product.price}
+                color={product.color}
+              />
+            </Link>
+          ))}
+        </div>
+        <Pagination
+          totalItems={products.length}
+          itemsPerPage={productsPerPage}
+          currentPage={page}
+          onPageChange={handlePageChange}
+        />
       </div>
     </div>
-  </div>
-
-
   );
 };
 
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,39 +1,34 @@
-// import {
-//     Pagination,
-//     PaginationContent,
-//     PaginationEllipsis,
-//     PaginationItem,
-//     PaginationLink,
-//     PaginationNext,
-//     PaginationPrevious,
-//   } from "@/components/ui/pagination"
-  
-//   export function PaginationDemo() {
-//     return (
-//       <Pagination>
-//         <PaginationContent>
-//           <PaginationItem>
-//             <PaginationPrevious href="#" />
-//           </PaginationItem>
-//           <PaginationItem>
-//             <PaginationLink href="#">1</PaginationLink>
-//           </PaginationItem>
-//           <PaginationItem>
-//             <PaginationLink href="#" isActive>
-//               2
-//             </PaginationLink>
-//           </PaginationItem>
-//           <PaginationItem>
-//             <PaginationLink href="#">3</PaginationLink>
-//           </PaginationItem>
-//           <PaginationItem>
-//             <PaginationEllipsis />
-//           </PaginationItem>
-//           <PaginationItem>
-//             <PaginationNext href="#" />
-//           </PaginationItem>
-//         </PaginationContent>
-//       </Pagination>
-//     )
-//   }
-  
+import React from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+const Pagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  // Determine if it's the last page
+  const isLastPage = currentPage === totalPages;
+
+  return (
+    <div className="flex justify-center w-full pt-[4rem] gap-[5rem]">
+      <button onClick={handlePrevPage} disabled={currentPage === 1}>
+        <IoIosArrowBack size={25}/>
+      </button>
+      <button onClick={handleNextPage} disabled={isLastPage}>
+        <IoIosArrowForward size={25} color={isLastPage ? "grey" : "black"} />
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
